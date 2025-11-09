c89b9ba8361cf0ecd43b0a9431edae120cd9c2f8
Thanusree S N <snthanu101@gmail.com>
2025-11-09 14:07:31 +0530

    Fixed the image cycling interval in light.js to 500 milliseconds.

--- light.js
+++ light.js
@@ -1,2 +1,11 @@
-const img =["all","red","yellow","green"];
+const imgIds =["all","red","yellow","green"];
 let flow = 0;
+function changeColor()
+{
+    imgIds.forEach(id => {
+        document.getElementById(id).style.display = "none";
+    });
+    flow = (flow + 1) % imgIds.length;
+    document.getElementById(imgIds[flow]).style.display = "block";
+}
+setInterval(changeColor, 500);
